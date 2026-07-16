3ef55e6114c19c6a29308bf750b7932731604ab8
ranjithashivakumar11 <ranjithashivakumar1@gmail.com>
2026-07-16 09:19:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Delta Scanner Started")
+import requests
+import time
+
+PRODUCTS_URL = "https://api.india.delta.exchange/v2/products"
+TICKERS_URL = "https://api.india.delta.exchange/v2/tickers"
+CANDLES_URL = "https://api.india.delta.exchange/v2/history/candles"
+
+print("================================")
+print("     DELTA SCANNER STARTED")
+print("================================")
